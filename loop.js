283923ac7e67251ad78522799af98d470a164fb1
sohel khan <mainsohelhu@gmail.com>
2026-06-10 08:48:30 +0530

    new loop added

--- loop.js
+++ loop.js
@@ -4,3 +4,20 @@ for (let i = 0; i < text.length; i++) {
     console.log(text[i]);
     
 }
+
+
+function max_num(num){
+    const large_num = 0
+
+    for (let n = 0; n < num.length; n++) {
+        if(n > large_num){
+            large_num === n
+            return large_num
+        }
+        
+    }
+}
+
+nums = [1,2,3,4,5,10]
+
+console.log(max_num(nums))
